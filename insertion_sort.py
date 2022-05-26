2c2da40d79a4b55f7514f63c827c0fefe0d50b7b
2M (he/him) <92114044+Matheysmota@users.noreply.github.com>
2022-05-25 22:49:05 -0300

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,5 +1,5 @@
 
-
+#Como o algoritmo ficaria no primeiro looping
 
 myList = [5, 4 , 4, 6, 7, 15, 1, 3] --> [5, 5] -> 4,5....
 
@@ -11,4 +11,4 @@ for i in range(1, n): #i variando de (1 até n) e somando de 1 em 1
         myList[j + 1] = myList[j] # a segunda posição (5) recebe o valor da primeira posição (4)
         j = j - 1 #a posição 0 recebe o valor -1
     myList[j+1] = key #a primeira posição recebe 4
-    print(myList)
+    print(myList) #imprime a lista após a primeira ordeção
